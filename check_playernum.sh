8ad6f3ffdc3aedc1d36d9851b8c6e3529f534c56
reanisz <reannml@gmail.com>
2017-08-29 14:57:47 +0900

    起動してすぐ停止しないようにした

--- check_playernum.sh
+++ check_playernum.sh
@@ -20,6 +20,14 @@ DATE=`date +%Y-%m-%d-%H:%M:%S`
 
 echo "$DATE $PLAYER_NUM" >> /tmp/log_minecraft_playernum
 
+ROW_NUM=`tail -n 6 /tmp/log_minecraft_playernum | wc -l`
+
+if [[ 6 -ne $ROW_NUM ]]; then
+    exit 
+fi
+
+exit
+
 if tail -n 6 /tmp/log_minecraft_playernum | cut -d ' ' -f 2 | grep -v '^0$' > /dev/null 2> /dev/null; then
     # 0じゃない行がある = 計測6回以内に1人以上参加者が居た
     :
