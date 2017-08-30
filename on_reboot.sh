36f4aacd4e18435b93c0c5f7ded4f6ddf1dc97fb
reanisz <reannml@gmail.com>
2017-08-30 16:52:46 +0900

    reboot時にpullするように

--- on_reboot.sh
+++ on_reboot.sh
@@ -3,6 +3,10 @@
 ROOT=$(cd $(dirname $0);pwd)
 cd $ROOT
 
+# 最新の設定に更新する
+git fetch
+git reset --hard origin/$(git rev-parse --abbrev-ref HEAD)
+
 rm /tmp/log_minecraft_playernum
 
 # instanceを作り直すと /etc/hosts が変わるかもしれない
